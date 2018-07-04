9700f9c53d1d7aced440d92ee459805f3c664a62
Małgorzata Grajewska <malgorzata.grajewska@hotmail.co.kr>
2018-07-04 17:14:18 +0200

    Add info carousel (#60)

--- app/pages/home/home-page.ts
+++ app/pages/home/home-page.ts
@@ -1,5 +1,5 @@
 import * as frameModule from "ui/frame";
-import { NavigatedData, Page } from "ui/page";
+import { NavigatedData, Page, EventData } from "ui/page";
 import { HomeViewModel } from "./home-view-model";
 
 export function onNavigatingTo(args: NavigatedData) {
@@ -12,4 +12,10 @@ export function startTap() {
         moduleName: "pages/background-gallery/background-gallery-page",
         transition: { name: "slide" },
     });
+}
+
+export function showAppInfo(args: EventData) {
+    const page = args.object as Page;
+    const model = page.bindingContext as HomeViewModel;
+    model.set('appInfoVisibility', !model.appInfoVisibility);
 }
--- app/pages/home/home-view-model.ts
+++ app/pages/home/home-view-model.ts
@@ -1,6 +1,8 @@
 import { Observable } from "data/observable";
 
 export class HomeViewModel extends Observable {
+    public appInfoVisibility: boolean = false;
+    
     constructor() {
         super();
     }
